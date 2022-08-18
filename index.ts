802cb0e55e549b45d17d05a8f7c1917fc3ce09ed
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-08-18 12:42:43 +0500

    ✨ Basic types

---
+++ index.ts
@@ -0,0 +1,25 @@
+let id :number =1;
+
+let my_nama: string ="shaukat"
+
+// let names: number []= [1,434,23];
+
+//cannot push string to numbers
+// names.push("hello");
+
+
+// Tuple 
+let person:  [number, string , boolean]=[4,'sd', true]
+
+
+
+// Tuple Array
+
+let emp:[number , string][];
+
+// emp.push(121,"shaukat")
+
+emp=[
+    [1,"sda"],
+    [2,"something"]
+]
